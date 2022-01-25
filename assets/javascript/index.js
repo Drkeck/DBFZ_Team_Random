80f861a65a1899157fd73129572064a913e0d92e
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-01-24 16:25:53 -0800

    added a thing for assists randomization

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -9,6 +9,53 @@ let pointC = {name: "", num: 0, assist: ""};
 let midC = {name: "", num: 0, assist: "" };
 let anchorC = {name: "", num: 0, assist: ""};
 
+function printTeam() {
+    // this is where everything gets rendered, starting with point characters.
+    var charTitle1 = document.createElement("h1")
+    var titleText1 = document.createTextNode(pointC.name);
+    charTitle1.appendChild(titleText1);
+    point.replaceChildren(charTitle1);
+
+    var charPort1 = document.createElement("img")
+    charPort1.src = "./assets/images/" + pointC.num + ".png"
+    point.appendChild(charPort1)
+
+    var assistHtml = document.createElement("h3");
+    var assistText = document.createTextNode(pointC.assist)
+    assistHtml.appendChild(assistText)
+    point.appendChild(assistHtml);
+
+    // next mid characters.
+    var charTitle2 = document.createElement("h1")
+    var titleText2 = document.createTextNode(midC.name);
+    charTitle2.appendChild(titleText2);
+    mid.replaceChildren(charTitle2);
+
+    var charPort2 = document.createElement("img")
+    charPort2.src = "./assets/images/" + midC.num + ".png"
+    mid.appendChild(charPort2)
+
+    var assistHtml = document.createElement("h3");
+    var assistText = document.createTextNode(midC.assist)
+    assistHtml.appendChild(assistText)
+    mid.appendChild(assistHtml);
+
+    // and finally the anchor, i know a lot of the team terminology may be lost on some reading this its just a dbfz thing.
+    var charTitle3 = document.createElement("h1")
+    var titleText3 = document.createTextNode(anchorC.name);
+    charTitle3.appendChild(titleText3);
+    anchor.replaceChildren(charTitle3);
+
+    var charPort3 = document.createElement("img")
+    charPort3.src = "./assets/images/" + anchorC.num + ".png"
+    anchor.appendChild(charPort3)
+
+    var assistHtml = document.createElement("h3");
+    var assistText = document.createTextNode(anchorC.assist)
+    assistHtml.appendChild(assistText)
+    anchor.appendChild(assistHtml);
+}
+
 const ButtonHandler = (e) => {    
     let newRoaster = roaster.map(ele => ele.name)
 
@@ -43,37 +90,9 @@ const ButtonHandler = (e) => {
         }
     }
 
-
-    
-
-    var charTitle1 = document.createElement("h1")
-    var titleText1 = document.createTextNode(pointC.name);
-    charTitle1.appendChild(titleText1);
-    point.replaceChildren(charTitle1);
-
-    var charPort1 = document.createElement("img")
-    charPort1.src = "./assets/images/" + pointC.num + ".png"
-    point.appendChild(charPort1)
-
-    var charTitle2 = document.createElement("h1")
-    var titleText2 = document.createTextNode(midC.name);
-    charTitle2.appendChild(titleText2);
-    mid.replaceChildren(charTitle2);
-
-    var charPort2 = document.createElement("img")
-    charPort2.src = "./assets/images/" + midC.num + ".png"
-    mid.appendChild(charPort2)
-
-    var charTitle3 = document.createElement("h1")
-    var titleText3 = document.createTextNode(anchorC.name);
-    charTitle3.appendChild(titleText3);
-    anchor.replaceChildren(charTitle3);
-
-    var charPort3 = document.createElement("img")
-    charPort3.src = "./assets/images/" + anchorC.num + ".png"
-    anchor.appendChild(charPort3)
-
-
+    printTeam()
 }
+
+
 
 btn.addEventListener("click", ButtonHandler)
